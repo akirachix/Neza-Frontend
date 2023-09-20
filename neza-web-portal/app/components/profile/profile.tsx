c9640de8362f0695e7c2cab3edd91c0a9bcd72eb
Wambui-Mwangi <mwangiestherwambuii@gmail.com>
2023-09-20 20:12:15 +0300

    updates

--- neza-web-portal/app/components/profile/profile.tsx
+++ neza-web-portal/app/components/profile/profile.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <div className="bg-white p-8">
-      <h1 className="text-2xl font-semibold ml-40 mb-10">My Account</h1>
+    <div className=" flex bg-white p-8">
+    <div className='ml-40'>
+      <h1 className="text-2xl font-semibold ml-40">My Account</h1>
       <div className="max-w-md mx-auto ">
-        <div className="ml-10">
-          <div className="shrink-0">
+        <div className="pl-20">
+          <div className="shrink-0 ml-20">
             <img
             src="/kanini.jpg"
-            width={250}
+            width={200}
             alt="profile"
             className='rounded-full'/>
           </div>
@@ -21,8 +22,8 @@ const Profile = () => {
           </div>
         </div>
       </div>
-      <form className='ml-20 pl-20'>
-      <div className="mb-4 mb-20">
+      <form className='ml-10  pl-20'>
+      <div className="mb-10">
         <label htmlFor="organizationName" className="block text-gray-700 mb-2">
           Organization Name:
         </label>
@@ -30,63 +31,73 @@ const Profile = () => {
           type="text"
           id="organizationName"
           name="organizationName"
-          className="px-2 py-0 border-b border-green-600 rounded-md focus:outline-none focus:border-yellow-600"
+          className="w-full px-16 py-2 border-b border-green-600 focus:outline-none focus:border-yellow-600"
           required
         />
       </div>
-      <div className="mb-4 mb-20">
-        <label htmlFor="organizationName" className="block text-gray-700 mb-2">
+      <div className="mb-10  ">
+        <label htmlFor="EmailAddress" className="block text-gray-700">
           E-mail Address:
         </label>
         <input
           type="email"
           id="e-mail"
           name="e-mail"
-          className="px-4 py-0 border-b border-green-600 rounded-md focus:outline-none focus:border-yellow-600"
+          className="px-16 py-0 border-b border-green-600 focus:outline-none focus:border-yellow-600"
           required
         />
       </div>
-      <div className="mb-4 mb-20">
-        <label htmlFor="organizationName" className="block text-gray-700 mb-2">
-          About:
+      <div className="mb-10">
+        <label htmlFor="organizationType" className="block text-gray-700 mb-2">
+          Organization Type:
         </label>
         <input
           type="text"
           id="about"
           name="about"
-          className="px-4 py-0 border-b border-green-600 rounded-md focus:outline-none focus:border-yellow-600"
+          className="px-16 py-0 border-b border-green-600 focus:outline-none focus:border-yellow-600"
           required
         />
       </div>
-      <div className="mb-4 mb-20">
-      <h1 className="text-2xl font-semibold mb-10">Change Password</h1>
-        <label htmlFor="organizationName" className="block text-gray-700 mb-2">
-          New Password:
+      <div className="mb-10">
+      <label htmlFor="PhoneNumber" className="block text-gray-700 mb-2">
+          Phone Number:
         </label>
         <input
           type="password"
           id="about"
           name="about"
-          className="px-4 py-0 border-b mb-10 border-green-600 rounded-md focus:outline-none focus:border-yellow-600"
+          className="px-16 py-0 border-b mb-10 border-green-600 focus:outline-none focus:border-yellow-600"
           required
         />
-        <label htmlFor="organizationName" className="block text-gray-700 mb-2">
-          Confirm Password:
+        <label htmlFor="Website" className="block text-gray-700">
+          Website:
         </label>
         <input
           type="password"
           id="about"
           name="about"
-          className="px-4 py-0 border-b border-green-600 rounded-md focus:outline-none focus:border-yellow-600"
+          className="px-16 py-0 border-b border-green-600 focus:outline-none focus:border-yellow-600"
+          required
+        />
+        <label htmlFor="Password" className="block text-gray-700 mt-10">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="about"
+          name="about"
+          className="px-16 py-0 border-b border-green-600 focus:outline-none focus:border-yellow-600"
           required
         />
       </div>
       <button
           type="submit"
-          className="w-40 ml-50 px-1 py-4 bg-green-400 border border-green-600 text-white text-2xl rounded-xl hover:bg-green-600 hover:text-white focus:outline-none focus:bg-green-600">
+          className="w-60 h-14 ml-1 px-1 py-4 bg-green-500 border border-green-600 text-white text-2xl rounded-xl hover:bg-green-600 hover:text-white focus:outline-none focus:bg-green-600">
           Done
         </button>
       </form>
+      </div>
     </div>
   );
 };
--- neza-web-portal/app/page.tsx
+++ neza-web-portal/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import SideBar from './components/sidebar/sidebar'
+import Profile from './components/profile/profile'
 
 export default function Home() {
   return (
     <div>
-      <SideBar/>
+      <Profile/>
 
     </div>
     
